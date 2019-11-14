8b5b4841eb49fd5bf1c7be00396e0d6a21facceb
Zuzanav <zuzanavolny@gmail.com>
2019-11-14 14:57:58 -0800

    troubleshooting

--- server.js
+++ server.js
@@ -4,9 +4,6 @@ const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// Define middleware here
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
@@ -16,5 +13,4 @@ app.use(routes);
 
 // Start the API server
 app.listen(PORT, function() {
-  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
 });
